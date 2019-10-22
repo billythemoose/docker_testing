8ef728651f1e97e8110e56e466e55b1ea95bfe14
billythemoose <mdiegothomas@gmail.com>
2019-10-22 00:26:05 -0700

    React structure reformatting and adding new components to be rendered Added input button to open file window (no actions)

--- composeexample/frontend/src/components/App.js
+++ composeexample/frontend/src/components/App.js
@@ -1,13 +1,65 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import DataProvider from "./DataProvider";
 import Table from "./Table";
 
+/*
 const App = () => (
-  <DataProvider endpoint="api/users/" 
-                render={data => <Table data={data} />} />
+  <div>
+    <DataProvider endpoint="api/users/" render={data => <Table data={data} />} />
+  </div>
+);
+*/
+
+
+/*
+const dataTable = (
+  <div>
+    <DataProvider endpoint="api/users/" render={data => <Table data={data} />} />
+    <button className="btn btn-default">Testing Button</button>
+  </div>
+);
+*/
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isAuthenticated: false
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <DataProvider endpoint="api/users/" render={data => <Table data={data} />} />
+        </div>
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6">
+              <form method="post" action="#" id="#">
+                <div class="form-group files">
+                  <label>Upload Your Transcript </label>
+                  <input type="file" class="form-control" multiple="" />
+                </div>
+              </form>
+            </div>
+            </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("app")
 );
 
+
+/*
 const wrapper = document.getElementById("app");
 
 wrapper ? ReactDOM.render(<App />, wrapper) : null;
+*/
