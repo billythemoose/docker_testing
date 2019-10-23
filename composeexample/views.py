57a3c6bdfa10589125d7079bdefed2426516ac1d
billythemoose <mdiegothomas@gmail.com>
2019-10-22 22:20:38 -0700

    Enabled conversion of PDF to pure text using pdfminer Returns single line response of entire text

--- composeexample/views.py
+++ composeexample/views.py
@@ -5,6 +5,14 @@ from rest_framework import generics
 from rest_framework import status
 from composeexample.models import Users
 from composeexample.serializers import UserSerializer, FilesSerializer
+from composeexample.models import Files
+
+import io
+import os.path
+from pdfminer.converter import TextConverter
+from pdfminer.pdfinterp import PDFPageInterpreter, PDFResourceManager
+from pdfminer.pdfpage import PDFPage
+from django.conf import settings
 
 class UserListCreate(generics.ListCreateAPIView):
     queryset = Users.objects.all()
@@ -16,11 +24,32 @@ class HelloWorld(APIView):
 
 class FileUploadView(APIView):
     parser_class = (FileUploadParser,)
+
+    def extract_text(self, pdf_path):
+        resource_manager = PDFResourceManager()
+        fake_file_handle = io.StringIO()
+        converter = TextConverter(resource_manager, fake_file_handle)
+        page_interpreter = PDFPageInterpreter(resource_manager, converter)
+        with open(pdf_path, 'rb') as fh:
+            for page in PDFPage.get_pages(fh, caching=True, check_extractable=True):
+                page_interpreter.process_page(page)
+            text = fake_file_handle.getvalue()
+        converter.close()
+        fake_file_handle.close()
+        if text:
+            return text
     
     def post(self, request, *args, **kwargs):
         file_serializer = FilesSerializer(data=request.data)
         if file_serializer.is_valid():
             file_serializer.save()
-            return Response(file_serializer.data, status=status.HTTP_201_CREATED)
+            print(str(request.FILES['file']))
+            print(settings.MEDIA_ROOT)
+            filepath = os.path.join(settings.MEDIA_ROOT, str(request.FILES['file']))
+            converted = self.extract_text(filepath)
+            return Response(converted, status=status.HTTP_201_CREATED)
+            # return Response(file_serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    
