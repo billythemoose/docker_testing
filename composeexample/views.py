14463a32a595450a6debbc5d6c1a3bb9e7354129
billythemoose <mdiegothomas@gmail.com>
2019-11-24 12:02:42 -0800

    Added PDF parsing to backend

--- composeexample/views.py
+++ composeexample/views.py
@@ -27,8 +27,10 @@ class HelloWorld(APIView):
 class FileUploadView(APIView):
     parser_class = (FileUploadParser,)
     
-    def extract_text_by_page(self, pdf_path):
+    def extract_text(self, pdf_path):
         try:
+            '''
+            # Original file handler
             with open(pdf_path, 'rb') as fh:
                 for page in PDFPage.get_pages(fh, 
                                             caching=True,
@@ -39,15 +41,51 @@ class FileUploadView(APIView):
                     page_interpreter = PDFPageInterpreter(resource_manager, converter)
                     page_interpreter.process_page(page)
                     text = fake_file_handle.getvalue()
+            '''
+            # Pmx990 file handler
+            with open(pdf_path, 'rb') as fh:
+                for page in PDFPage.get_pages(fh, caching=True, check_extractable=True):
+                    resource_manager = PDFResourceManager()
+                    fake_file_handle = io.StringIO()
+                    converter = TextConverter(resource_manager, fake_file_handle)
+                    page_interpreter = PDFPageInterpreter(resource_manager, converter)
+                    page_interpreter.process_page(page)
+                    text = fake_file_handle.getvalue()
+                    #yield text
+                    # close open handles
+                    converter.close()
+                    fake_file_handle.close()
+
+            #yield text
+            # close open handles
+            # converter.close()
+            # fake_file_handle.close()
+            # split_text = str(text).split()
+
+            # regex for pulling courses 
+            reg = "\s[A-Z]{2,5}&?\s+[0-9][0-9][0-9]\s"
+            clist = re.findall(reg,text)
+            #print(re.findall(reg,text))
+            for index,element in enumerate(clist):
+                element = element.replace(" ","")
+                clist[index] = element
+                print(element)
+            #print(clist)
+            jsFormat = json.dumps(clist)
+            # print(type(jsFormat))
+            # from django.core import serializers
+            # print(serializers.serialize("json",jsFormat))
+            if jsFormat:
+                return jsFormat
         except Exception as err:
             print(err)
-            converter.close()
-            fake_file_handle.close()
-        else:
-            yield text
-                    # close open handles
-            converter.close()
-            fake_file_handle.close()
+            # converter.close()
+            # fake_file_handle.close()
+        # else:
+            # yield text
+            # close open handles
+            # converter.close()
+            # fake_file_handle.close()
     serializer_class = UserSerializer
 
     def convert_to_list(self,text):
@@ -80,11 +118,15 @@ class FileUploadView(APIView):
     def post(self, request, *args, **kwargs):
         file_serializer = FilesSerializer(data=request.data)
         if file_serializer.is_valid():
-            file_serializer.save()
-            print(str(request.FILES['file']))
-            print(settings.MEDIA_ROOT)
-            filepath = os.path.join(settings.MEDIA_ROOT, str(request.FILES['file']))
+            retuned_filename = file_serializer.save()
+            print(str(retuned_filename))
+            # print('File Name: ' + str(request.FILES['file']))
+            print('File Name: ' + str(retuned_filename))
+            print('Media Root: ' + settings.MEDIA_ROOT)
+            filepath = os.path.join(settings.MEDIA_ROOT, str(retuned_filename))
+            print('File Path: ' + filepath)
             converted = self.extract_text(filepath)
+            print(converted)
             #converted = self.extract_json(filepath, str(request.FILES['file'])) 
             return Response(converted, status=status.HTTP_201_CREATED)
             # return Response(file_serializer.data, status=status.HTTP_201_CREATED)
